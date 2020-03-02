46dfc1690f7629ec1bd0a497697f9c1716b495cd
Wren Steitle <lauren.steitle@gmail.com>
2020-03-02 12:11:04 -0700

    Final submit

--- app/poros/restaurant.rb
+++ app/poros/restaurant.rb
@@ -1,7 +1,10 @@
 class Restaurant
 
   def find_restaurant(food, location)
-    restaurant_hash = Faraday.get("https://api.yelp.com/v3/businesses/search?Bearer=#{ENV['YELP_KEY']}&location=#{location}&term=restaurant,#{food}")
+    conn = Faraday.new('https://api.yelp.com/v3/businesses/search?') do |f|
+      f.headers ("authorization": "Bearer=#{ENV['YELP_KEY']}")
+    end
+    restaurant_hash = conn.get("&location=#{location}&term=restaurant,#{food}")
     restaurant = JSON.parse(restaurant_hash.body)
   end
 end
