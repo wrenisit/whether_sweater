b7e2bed44ec14ba7818e999400ebf7df3064361f
Wren Steitle <lauren.steitle@gmail.com>
2020-03-03 13:43:46 -0700

    Complete antipode location coordinates

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::AntipodeController < ApplicationController
 
   def create
+    antipode = AntipodeFacade.new(params[:location]).antipode_coordinates
 
   end
 end
---
+++ app/facade/antipode_facade.rb
@@ -0,0 +1,23 @@
+class AntipodeFacade < ForecastFacade
+  def initialize(loc)
+    @location = loc
+  end
+
+  def antipode_coordinates
+    coordinates_hash = request_anti
+    coordinates = JSON.parse(coordinates_hash.body)["data"]["attributes"]
+  end
+
+  private
+
+  def conn
+    Faraday.new('http://amypode.herokuapp.com/api/v1') do |f|
+      f.headers["api_key"] = ENV['AMYPODE_KEY']
+    end
+  end
+
+  def request_anti
+    location_hash = geocode
+    conn.get("antipodes?lat=#{location_hash[:lat]}&long=#{location_hash[:lon]}")
+  end
+end
