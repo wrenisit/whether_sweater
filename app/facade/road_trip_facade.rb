7e5a926940a56c3efd7a32ea74c01924aa16f067
Wren Steitle <lauren.steitle@gmail.com>
2020-03-03 19:43:21 -0700

    create initialization for road trips

--- app/facade/road_trip_facade.rb
+++ app/facade/road_trip_facade.rb
@@ -1,5 +1,10 @@
 class RoadTripFacade
   def initialize(origin, destination)
-    
+    @origin = origin
+    @destination = destination
+  end
+
+  def make_data( )
+    RoadTrip.new(@origin, @destiination, travel_time, condensed_forecast)
   end
 end
