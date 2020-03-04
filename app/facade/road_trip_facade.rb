a19778b43aba5302e32ddb544d4de8d0024162ff
Wren Steitle <lauren.steitle@gmail.com>
2020-03-04 08:15:23 -0700

    Almost finished

--- app/facade/road_trip_facade.rb
+++ app/facade/road_trip_facade.rb
@@ -4,7 +4,7 @@ class RoadTripFacade
     @destination = destination
   end
 
-  def make_data(travel_time, condensed_forecast_future)
+  def make_data
     RoadTrip.new(@origin, @destiination, travel_time, condensed_forecast)
   end
 
@@ -18,6 +18,13 @@ class RoadTripFacade
   end
 
   def arrival_time
-
+    # geocoded = GeoService.new.find(@destination)
+    # longitude = geocoded["results"][0]["geometry"]["location"]["lng"]
+    # latitude = geocoded["results"][0]["geometry"]["location"]["lat"]
+    # geocode_obj = Geocode.new(latitude, longitude)
+    geocode_directions = geo.directions(@start, @end_location)
+    @travel_time = geocode_directions["routes"][0]["legs"][0]["duration"]["text"]
+    time = @travel_time.split(" ")
+    Time.now.to_i + (time[0].to_i * 60 * 60) + (time[2].to_i * 60)
   end
 end
