8edef610739c53002e68d0f35d82063582bc55c0
Wren Steitle <lauren.steitle@gmail.com>
2020-03-04 08:35:41 -0700

    Complete!

--- app/facade/road_trip_facade.rb
+++ app/facade/road_trip_facade.rb
@@ -5,27 +5,31 @@ class RoadTripFacade
   end
 
   def make_data
-    RoadTrip.new(@origin, @destiination, travel_time, condensed_forecast)
+    RoadTrip.new(@origin, @destiination, travel_time, condensed_forecast_future)
   end
 
   def travel_time
-    geo = GeoService.new.directions(@start, @end_location)
+    geo = GeoService.new.directions(@origin, @destination)
     geo["routes"][0]["legs"][0]["duration"]["text"]
   end
 
   def condensed_forecast_future
-    DarkskyService.new.forecast_condensed(@destination, arrival_time)
+    geocode = GeoService.new.find(@destination)
+    longitude = geocode["results"][0]["geometry"]["location"]["lng"]
+    latitude = geocode["results"][0]["geometry"]["location"]["lat"]
+    geohash = {lat: latitude, lon: longitude}
+    DarkskyService.new.forecast_condensed(geohash, arrival_time)
   end
 
   def arrival_time
-    traveling_time = travel_time
+    # traveling_time = travel_time
     # geocoded = GeoService.new.find(@destination)
     # longitude = geocoded["results"][0]["geometry"]["location"]["lng"]
     # latitude = geocoded["results"][0]["geometry"]["location"]["lat"]
     # geocode_obj = Geocode.new(latitude, longitude)
     # geocode_directions = geo.directions(@start, @end_location)
     # travel_time = geocode_directions["routes"][0]["legs"][0]["duration"]["text"]
-    time = traveling_time.split(" ")
+    time = travel_time.split(" ")
     Time.now.to_i + (time[0].to_i * 60 * 60) + (time[2].to_i * 60)
   end
 end
--- app/services/darksky_service.rb
+++ app/services/darksky_service.rb
@@ -6,7 +6,8 @@ class DarkskyService
   end
 
   def forecast_condensed(code, time)
-    forecast_hash = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARKSKY_KEY']}/#{code.latitude},#{code.longitude},#{time.to_i}")
+    forecast_resp = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARKSKY_KEY']}/#{code[:lat]},#{code[:lon]},#{time.to_i}")
+    forecast_hash = JSON.parse(forecast_resp.body)
     condensed_forecast = Hash.new
     condensed_forecast["summary"] = forecast_hash["currently"]["summary"]
     condensed_forecast["current_temperature"] = forecast_hash["currently"]["temperature"]
